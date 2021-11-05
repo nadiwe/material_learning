7444884d29dcf08382808cc3f3e937118a34aadf
nadiwe <nadia.west@hispeed.ch>
2021-11-05 09:18:06 +0100

    almost finished

--- datatest.js
+++ datatest.js
@@ -1,8 +1,10 @@
 //________________ml5
 let model;
 let targetLabel = 'C';
+let material;
 
 let state = 'collection';
+
 //________________ml5
 
 
@@ -19,7 +21,7 @@ let client;
 // For shiftr.io type in both username and password
 
 let creds = {
-    clientID: 'data', //HIER_______________________________
+    clientID: 'dataNblau', //HIER_______________________________
     userName: 'nadiaunivers', //HIER_______________________________
     password: 'MAjPLOJ6qo2p7Ydk' //HIER_______________________________
 };
@@ -27,18 +29,26 @@ let creds = {
 // topic to subscribe to when you connect
 // For shiftr.io, use whatever word you want 
 let topicPosition = 'pos';
-let topic = 'values';
-let topic2 = 'ledBlink';
+ let topicPosition2 = 'pos2';
+ let firstValues;
+ let secondValues;
+ let allValues;
+
+ let topic = 'buch';
+
+ let lastTimeSent = 0;
+const sendInterval = 100;
+
+
 
 
 // position of the circle
-let aPos, bPos, cPos, dPos, ePos, fPos, gPos, hPos, iPos, jPos;
+// let aPos, bPos, cPos, dPos, ePos, fPos, gPos, hPos, iPos, jPos, kPos;
 
 //button click
 let clicked = false;
 
-let lastTimeSent = 0;
-const sendInterval = 100;
+
 
 function setup() {
     createCanvas(400, 400);
@@ -68,43 +78,81 @@ function setup() {
   
  
    let options = {
-     inputs: ['a', 'b','c','d','e','f','g','h','i','j'],
+        inputs: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r'],
+        // inputs: ['a','b'],
      outputs: ['label'],
      task: 'classification',
      debug: 'true'
    };
+  //  console.log(options);
+
    model = ml5.neuralNetwork(options);
+   
+  //  model.loadData('mouse-notes.json');
+  //  state = 'prediction';
+  //  loadDataMaterial();
+   
    background(255);
       //__________________________ml5_________
 
 }
 
+// function loadDataMaterial(){
+//     model.loadData('mouse-notes.json');
+//     state = 'prediction';
+// console.log('hello');
+// }
+
 function draw() {
 background(0,255,0);
 textSize(32);
-//text('aPos: '+ aPos,100,100);
-if(targetLabel== 'W'){
-    text('material: Wood',10,300);
-   
+console.log(state);
+// if (millis() - lastTimeSent > sendInterval) {
+//   sendMqttMessage("wood", topic);
+//   lastTimeSent = millis();
+// }
 
+if(state == 'prediction'){
+    if( material == 'W'){
+        textSize(62);
+        text('Wood',10,300);
+        sendMqttMessage("Wood", topic);
+
+    }
+    else if ( material == 'P'){
+        textSize(62);
+        text('Paper',10,300);
+        sendMqttMessage("Paper", topic);
+    }
+    else{
+        text('???',10,300);
+        sendMqttMessage("???", topic);
+
+    }
+
+}else if(state == 'collection'){
+    if(targetLabel== 'W' ){
+        text('material: Wood',10,300);
+
+    
+    }
+    if(targetLabel== 'P'){
+        text('material: Paper' ,10,300);
+    
+    }else if(targetLabel== 'C'){
+        text('material: COLLECTING' ,10,300);
+    
+    }
 }
-if(targetLabel== 'P'){
-    text('material: Paper' ,10,300);
-
-}else if(targetLabel== 'C'){
-    text('material: ??' ,10,300);
-
 }
+
+
 
     
     
-}
 
-// called when the client connects
-function onConnect() {
-    localMsg.html('client is connected');
-    client.subscribe(topicPosition);
-}
+
+
 
 // called when the client loses its connection
 function onConnectionLost(response) {
@@ -114,25 +162,35 @@ function onConnectionLost(response) {
     }
 }
 
+// called when the client connects
+function onConnect() {
+    localMsg.html('client is connected');
+    client.subscribe(topicPosition);
+    client.subscribe(topicPosition2);
+}
+
 // called when a message arrives
 function onMessageArrived(message) {
-    remoteMsg.html(message.payloadString);
-    
-    // assume the message payload is a JSON object  {"x":xPos, "y":yPos}
-    // parse it and use the X and Y:
-    var acc = JSON.parse(message.payloadString);
-    aPos = acc.a;
-    bPos = acc.b;
-    cPos = acc.c;
-    dPos = acc.d;
-    ePos = acc.e;
-    fPos = acc.f;
-    gPos = acc.g;
-    hPos = acc.h;
-    iPos = acc.i;
-    jPos = acc.j;
-     
-    
+  
+   if (message.destinationName  == "pos") {
+
+      firstValues = split(message.payloadString, " ");
+  
+
+  
+   } else if (message.destinationName  == "pos2"){
+     secondValues = split(message.payloadString, " ");
+   
+
+
+  } 
+  // console.log("firstValues" + " " + firstValues);
+  // console.log("secondValues" + " " + secondValues)
+
+       allValues = firstValues.concat(secondValues);  
+    //  console.log("allValues" + " " + allValues)
+     //remoteMsg.html(message.payloadString);
+
 }
 
 
@@ -140,15 +198,16 @@ function onMessageArrived(message) {
 //_____________________ml5
 
 function keyPressed() {
-    if (key == 't') {
-      state = 'training';
-      console.log('starting training');
-      model.normalizeData();
-      let options = {
-        epochs: 200
-      };
-      model.train(options, whileTraining, finishedTraining);
-    } else if (key == 's') {
+  if (key == 't') {
+    state = 'training';
+    console.log('starting training');
+     model.normalizeData();
+    let options = {
+      epochs: 200
+    };
+    model.train(options, whileTraining, finishedTraining);
+    } 
+    else if (key == 's') {
       model.saveData('mouse-notes');
     } 
     else {
@@ -167,19 +226,33 @@ function keyPressed() {
   }
   
   function mousePressed() {
-    let inputs = {
-      a: aPos,
-      b: bPos,
-      c: cPos,
-      d: dPos,
-      e: ePos,
-      f: fPos,
-      g: gPos,
-      h: hPos,
-      i: iPos,
-      j: jPos
+ 
+     let inputs = {
+     
+  a: allValues[0],
+  b: allValues[1],
+  c: allValues[2],
+  d: allValues[3],
+  e: allValues[4],
+  f: allValues[5],
+  g: allValues[6],
+  h: allValues[7],
+  i: allValues[8],
+  j: allValues[9],
+  k: allValues[10],
+  l: allValues[11],
+  m: allValues[12],
+  n: allValues[13],
+  o: allValues[14],
+  p: allValues[15],
+  q: allValues[16],
+  r: allValues[17]
+
+
     };
-  
+    
+    // console.log('inputs :'+inputs);
+
     if (state == 'collection') {
       let target = {
         label: targetLabel
@@ -194,7 +267,10 @@ function keyPressed() {
   
     } else if (state == 'prediction') {
       model.classify(inputs, gotResults);
-      console.log(state);
+      // console.log(inputs);
+            console.log(targetLabel);
+
+      console.log('where i am')
 
     }
   }
@@ -206,10 +282,26 @@ function keyPressed() {
     }
     console.log(results);
   
-    let label = results[0].label;
-    text(label, mouseX, mouseY);
-  
+     let material = results[0].label;
+     text(material, 10, 10);
+
   }
+
+// called when you want to send a message:
+function sendMqttMessage(msg, tpc) {
+  // if the client is connected to the MQTT broker:
+  if (client.isConnected()) {
+      // start an MQTT message:
+      message = new Paho.MQTT.Message(JSON.stringify(msg));
+      // choose the destination topic:
+      message.destinationName = tpc;
+      // send it:
+      client.send(message);
+      // print what you sent:
+      localMsg.html('I sent: ' + message.payloadString);
+  }
+}
+
 
 
   
